962abf9152c1f308f9ed2a27c0cfddfc81eaa601
amandaguan-ag <amandaguan1314@gmail.com>
2023-04-21 18:14:55 -0700

    pass getprice and getdeal test for bread

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,17 +4,12 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    private int price_;
-    private int deal_;
-    public Bread()
-    {
-      price_ = 5;
-      deal_ = 3;
-    }
-   
+    public int Price { get; } = 5;
+    public int Deal { get; } = 3; 
+
     public int CalculateCost(int numBread)
     {
-      int breadCost = (numBread / deal_) * (2 * price_) + (numBread % deal_) * price_;
+      int breadCost = (numBread / Deal) * (2 * Price) + (numBread % Deal) * Price;
       return breadCost;
     }
   }
