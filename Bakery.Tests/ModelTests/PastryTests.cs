370858dba76e6fd6ce913e288ca3c208e7d1723b
amandaguan-ag <amandaguan1314@gmail.com>
2023-04-21 17:51:42 -0700

    fail pastry test

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -12,5 +12,19 @@ namespace Bakery.Tests
           Pastry newPastry = new Pastry();
           Assert.AreEqual(typeof(Pastry), newPastry.GetType());
         }
+        [TestMethod]
+        public void CalculateCost_CalculateCostOfPastry_Int()
+        {
+            // Arrange
+            Pastry newPastry = new Pastry();
+            int numPastry = 4;
+            int expectedCost = 7;
+
+            // Act
+            int actualCost = newPastry.CalculateCost(numPastry);
+
+            // Assert
+            Assert.AreEqual(expectedCost, actualCost);
+        }
     }
 }
